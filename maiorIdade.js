695bc96bfa183ac917f4ccbbd24bb2c64d123a3e
Programação PHP 3612952.2024.4 <36129522024.4@senacrjedu.local>
2024-09-20 21:23:05 -0300

    Primeiro commit

---
+++ maiorIdade.js
@@ -0,0 +1,13 @@
+// ira digitar a sua idade e
+//caso a idade seja maior ou igual a 18
+//terá como resposta "Maior Idade"
+//caso contrário , "Menor de Idade"
+
+idade = 23
+
+if(idade >= 18){
+   console.log('Maior de Idade') 
+}
+else{
+    console.log('Menor de Idade')
+}
---
+++ primeiro.js
@@ -0,0 +1,21 @@
+x = '11'
+
+// Errado, variavel começa com letra minuscula
+Conta = 'corrente'
+
+//certo
+conta = 'corrente'
+
+//Não pode ->iniciar com numero
+_1c =10
+
+//conta corrente ='10101010'
+
+//snake case
+conta_corrente = '10101010'
+
+//Camel case
+contaCorrente = '10101010'
+
+//Pascal case
+ContaCorrente = '10101010'
